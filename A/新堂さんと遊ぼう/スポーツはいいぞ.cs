25588a7640d616e477540a4bc2eb3d085bc51b63
ymCompany <60121246+184ym2@users.noreply.github.com>
2020-12-07 09:25:45 +0900

    20201207

---
+++ A/新堂さんと遊ぼう/スポーツはいいぞ.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace 新堂さんと遊ぼう
+{
+    class スポーツはいいぞ
+    {
+        public void show()
+        {
+            Console.WriteLine("新堂「スポーツはいいぞ」");
+            Console.ReadKey();
+            sort("新堂「スホ゜ーツはいいそ゛」");
+            Console.ReadKey();
+        }
+
+        private static void sort(string sport)
+        {
+            //char型配列に変換
+
+            var charArray = sport.ToCharArray();
+
+            var charArray2 = Regex.Split(sport, @"\p{P}");
+
+            //シャッフルしてchar型配列に戻す　条件をつける
+            //「」は前後になる
+            //半濁点は[ホ]または[は]のみ　濁点は[ス][ホ][ツ][は][そ]のみ
+
+            var randomArray = charArray.OrderBy(i => Guid.NewGuid()).ToArray();
+            
+            //一文字ずつstring型に変換
+            foreach (var c in randomArray)
+            {
+                c.ToString();
+            }
+
+            //表示
+            Console.WriteLine(randomArray);
+        }
+    }
+}
---
+++ CsharpObjectの複製Sample/CsharpObjectの複製Sample/ShallowCopy/ShallowCopy_constructor.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CsharpObjectの複製Sample
+{
+    internal class ShallowCopy_Constructor
+    {
+        /// <summary>
+        /// 構造体(struct)のシャロ―コピー：コピーコンストラクタ
+        /// 値型/string型は変更の影響を受けない
+        /// </summary>
+        public void stractCopy_Constructor()
+        {
+            Console.WriteLine("\r\n------2-A 構造体(struct)のシャロ―コピー：コピーコンストラクタ------\r\n");
+
+            #region newする
+
+            var original = new SampleStruct
+                           {
+                               Id = 10,
+                               Name = "AAA",
+                               Ids = new[]
+                                     {
+                                         1,
+                                         2,
+                                         3
+                                     },
+                               Names = new[]
+                                       {
+                                           "A",
+                                           "B",
+                                           "C"
+                                       }
+                           };
+
+            #endregion
+
+            Console.WriteLine("---SampleStruct 構造体のメンバーに int型/string型/配列 を含め、そのメンバーを表示する---");
+            var copy = new SampleStruct(original);
+
+            Action 構造体のメンバー = () =>
+                                  {
+                                      Extensions.値をコピー(original.Id, original.Name,
+                                                       string.Join(", ", original.Ids), string.Join(", ", original.Names),
+                                                       copy.Id, copy.Name,
+                                                       string.Join(", ", copy.Ids), string.Join(", ", copy.Names));
+
+                                      original.Id = 99;
+                                      original.Name = "Hello, " + "World!";
+                                      original.Ids[0] = 9;
+                                      original.Names[0] = "あ";
+                                      Extensions.値を変更(Extensions.Original, original.Id, original.Name,
+                                                      string.Join(", ", original.Ids), string.Join(", ", original.Names),
+                                                      copy.Id, copy.Name,
+                                                      string.Join(", ", copy.Ids), string.Join(", ", copy.Names));
+                                  };
+            構造体のメンバー();
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 構造体(struct)のシャロ―コピー：コピーコンストラクタ
+        /// 値型/string型は変更の影響を受けないが、参照型メンバの値型は影響を受ける
+        /// </summary>
+        public void stractCopy_Constructor2()
+        {
+            Console.WriteLine("\r\n------2-A 構造体(struct)のシャロ―コピー：コピーコンストラクタ------\r\n");
+
+            #region newする
+
+            var original = new SampleStruct2
+                           {
+                               SampleStruct = new SampleStruct
+                                              {
+                                                  Id = 10,
+                                                  Name = "AAA",
+                                                  Ids = new[]
+                                                        {
+                                                            1,
+                                                            2,
+                                                            3
+                                                        },
+                                                  Names = new[]
+                                                          {
+                                                              "A",
+                                                              "B",
+                                                              "C"
+                                                          }
+                                              },
+                               SampleClass = new SampleClass
+                                             {
+                                                 Id = 10,
+                                                 Name = "AAA",
+                                                 Ids = new[]
+                                                       {
+                                                           1,
+                                                           2,
+                                                           3
+                                                       },
+                                                 Names = new[]
+                                                         {
+                                                             "A",
+                                                             "B",
+                                                             "C"
+                                                         }
+                                             }
+                           };
+
+            #endregion
+
+            Console.WriteLine("---SampleStruct2 構造体のメンバーに SampleStruct 構造体と SampleClass クラスを含め、そのメンバーを表示する---\r\n");
+            var copy = new SampleStruct2(original);
+
+            Action 構造体のメンバーSampleStruct構造体 = () =>
+                                                 {
+                                                     Console.WriteLine("---メンバー：SampleStruct 構造体---");
+                                                     Extensions.値をコピー(original.SampleStruct.Id, original.SampleStruct.Name,
+                                                                      string.Join(", ", original.SampleStruct.Ids), string.Join(", ", original.SampleStruct.Names),
+                                                                      copy.SampleStruct.Id, copy.SampleStruct.Name,
+                                                                      string.Join(", ", copy.SampleStruct.Ids), string.Join(", ", copy.SampleStruct.Names));
+
+                                                     original.SampleStruct.Id = 99;
+                                                     original.SampleStruct.Name = "Hello, " + "World!";
+                                                     original.SampleStruct.Ids[0] = 9;
+                                                     original.SampleStruct.Names[0] = "あ";
+                                                     Extensions.値を変更(Extensions.Original, original.SampleStruct.Id, original.SampleStruct.Name,
+                                                                     string.Join(", ", original.SampleStruct.Ids), string.Join(", ", original.SampleStruct.Names),
+                                                                     copy.SampleStruct.Id, copy.SampleStruct.Name,
+                                                                     string.Join(", ", copy.SampleStruct.Ids), string.Join(", ", copy.SampleStruct.Names));
+                                                 };
+
+            Action 構造体のメンバーSampleClassクラス = () =>
+                                                {
+                                                    Console.WriteLine("---メンバー：SampleClass クラス---");
+                                                    Extensions.値をコピー(original.SampleClass.Id, original.SampleClass.Name,
+                                                                     string.Join(", ", original.SampleClass.Ids), string.Join(", ", original.SampleClass.Names),
+                                                                     copy.SampleClass.Id, copy.SampleClass.Name,
+                                                                     string.Join(", ", copy.SampleClass.Ids), string.Join(", ", copy.SampleClass.Names));
+
+                                                    original.SampleClass.Id = 99;
+                                                    original.SampleClass.Name = "Hello, " + "World!";
+                                                    original.SampleClass.Ids[0] = 9;
+                                                    original.SampleClass.Names[0] = "あ";
+                                                    Extensions.値を変更(Extensions.Original, original.SampleClass.Id, original.SampleClass.Name,
+                                                                    string.Join(", ", original.SampleClass.Ids), string.Join(", ", original.SampleClass.Names),
+                                                                    copy.SampleClass.Id, copy.SampleClass.Name,
+                                                                    string.Join(", ", copy.SampleClass.Ids), string.Join(", ", copy.SampleClass.Names));
+                                                };
+
+            構造体のメンバーSampleStruct構造体();
+            構造体のメンバーSampleClassクラス();
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// クラス(class)のシャロ―コピー：コピーコンストラクタ
+        /// 値型/string型は変更の影響を受けない
+        /// </summary>
+        public void classCopy_Constructor()
+        {
+            Console.WriteLine("\r\n------2-B クラス(class)のシャロ―コピー：コピーコンストラクタ------\r\n");
+
+            #region newする
+
+            var original = new SampleClass
+                           {
+                               Id = 10,
+                               Name = "AAA",
+                               Ids = new[]
+                                     {
+                                         1,
+                                         2,
+                                         3
+                                     },
+                               Names = new[]
+                                       {
+                                           "A",
+                                           "B",
+                                           "C"
+                                       }
+                           };
+
+            #endregion
+
+            Console.WriteLine("---SampleClass クラスのメンバーに int型/string型/配列 を含め、そのメンバーを表示する---");
+            var copy = new SampleClass(original);
+
+            Action クラスのメンバー = () =>
+                                  {
+                                      Extensions.値をコピー(original.Id, original.Name,
+                                                       string.Join(", ", original.Ids), string.Join(", ", original.Names),
+                                                       copy.Id, copy.Name,
+                                                       string.Join(", ", copy.Ids), string.Join(", ", copy.Names));
+
+                                      original.Id = 99;
+                                      original.Name = "Hello, " + "World!";
+                                      original.Ids[0] = 9;
+                                      original.Names[0] = "あ";
+                                      Extensions.値を変更(Extensions.Original, original.Id, original.Name,
+                                                      string.Join(", ", original.Ids), string.Join(", ", original.Names),
+                                                      copy.Id, copy.Name,
+                                                      string.Join(", ", copy.Ids), string.Join(", ", copy.Names));
+                                  };
+
+            クラスのメンバー();
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// クラス(class)のシャロ―コピー：コピーコンストラクタ
+        /// 値型/string型は変更の影響を受けないが、参照型メンバの値型は影響を受ける
+        /// </summary>
+        public void classCopy_Constructor2()
+        {
+            Console.WriteLine("\r\n------2-B クラス(class)のシャロ―コピー：コピーコンストラクタ------\r\n");
+
+            #region newする
+
+            var original = new SampleClass2
+                           {
+                               SampleStruct = new SampleStruct
+                                              {
+                                                  Id = 10,
+                                                  Name = "AAA",
+                                                  Ids = new[]
+                                                        {
+                                                            1,
+                                                            2,
+                                                            3
+                                                        },
+                                                  Names = new[]
+                                                          {
+                                                              "A",
+                                                              "B",
+                                                              "C"
+                                                          }
+                                              },
+                               SampleClass = new SampleClass
+                                             {
+                                                 Id = 10,
+                                                 Name = "AAA",
+                                                 Ids = new[]
+                                                       {
+                                                           1,
+                                                           2,
+                                                           3
+                                                       },
+                                                 Names = new[]
+                                                         {
+                                                             "A",
+                                                             "B",
+                                                             "C"
+                                                         }
+                                             }
+                           };
+
+            #endregion
+
+            Console.WriteLine("---SampleClass2 クラスのメンバーに SampleStruct 構造体と SampleClass クラスを含め、そのメンバーを表示する---\r\n");
+            var copy = new SampleClass2(original);
+
+            Action クラスのメンバーSampleStruct構造体 = () =>
+                                                 {
+                                                     Console.WriteLine("---メンバー：SampleStruct 構造体---");
+                                                     Extensions.値をコピー(original.SampleStruct.Id, original.SampleStruct.Name,
+                                                                      string.Join(", ", original.SampleStruct.Ids), string.Join(", ", original.SampleStruct.Names),
+                                                                      copy.SampleStruct.Id, copy.SampleStruct.Name,
+                                                                      string.Join(", ", copy.SampleStruct.Ids), string.Join(", ", copy.SampleStruct.Names));
+
+                                                     original.SampleStruct.Id = 99;
+                                                     original.SampleStruct.Name = "Hello, " + "World!";
+                                                     original.SampleStruct.Ids[0] = 9;
+                                                     original.SampleStruct.Names[0] = "あ";
+                                                     Extensions.値を変更(Extensions.Original, original.SampleStruct.Id, original.SampleStruct.Name,
+                                                                     string.Join(", ", original.SampleStruct.Ids), string.Join(", ", original.SampleStruct.Names),
+                                                                     copy.SampleStruct.Id, copy.SampleStruct.Name,
+                                                                     string.Join(", ", copy.SampleStruct.Ids), string.Join(", ", copy.SampleStruct.Names));
+                                                 };
+
+            Action クラスのメンバーSampleClassクラス = () =>
+                                                {
+                                                    Console.WriteLine("---メンバー：SampleClass クラス---");
+                                                    Extensions.値をコピー(original.SampleClass.Id, original.SampleClass.Name,
+                                                                     string.Join(", ", original.SampleClass.Ids), string.Join(", ", original.SampleClass.Names),
+                                                                     copy.SampleClass.Id, copy.SampleClass.Name,
+                                                                     string.Join(", ", copy.SampleClass.Ids), string.Join(", ", copy.SampleClass.Names));
+
+                                                    original.SampleClass.Id = 99;
+                                                    original.SampleClass.Name = "Hello, " + "World!";
+                                                    original.SampleClass.Ids[0] = 9;
+                                                    original.SampleClass.Names[0] = "あ";
+                                                    Extensions.値を変更(Extensions.Original, original.SampleClass.Id, original.SampleClass.Name,
+                                                                    string.Join(", ", original.SampleClass.Ids), string.Join(", ", original.SampleClass.Names),
+                                                                    copy.SampleClass.Id, copy.SampleClass.Name,
+                                                                    string.Join(", ", copy.SampleClass.Ids), string.Join(", ", copy.SampleClass.Names));
+                                                };
+
+            クラスのメンバーSampleStruct構造体();
+            クラスのメンバーSampleClassクラス();
+            Console.ReadKey();
+        }
+    }
+}
